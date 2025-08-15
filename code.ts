f646f14ff0a418bd055cdf7512c115f226feef79
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-08-15 01:18:12 -0400

    return early if all nodes in selection have been found

--- code.ts
+++ code.ts
@@ -11,67 +11,73 @@ postSelectionCountUpdate()
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = (msg: { type: string; names: string[] }) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === "execute-bulk-rename") {
-    const names = msg.names
-    const nodes = getSortedSelection()
+	// One way of distinguishing between different types of messages sent from
+	// your HTML page is to use an object with a "type" property like this.
+	if (msg.type === "execute-bulk-rename") {
+		const names = msg.names
+		const nodes = getSortedSelection()
 
-    const numOperations = Math.min(names.length, nodes.length)
-    for (let i = 0; i < numOperations; i++) {
-      const currName = names[i]
-      const currNode = nodes[i]
+		const numOperations = Math.min(names.length, nodes.length)
+		for (let i = 0; i < numOperations; i++) {
+			const currName = names[i]
+			const currNode = nodes[i]
 
-      currNode.name = currName
-    }
+			currNode.name = currName
+		}
 
-    figma.notify(`Renamed ${numOperations} layers.`)
-  }
+		figma.notify(`Renamed ${numOperations} layers.`)
+	}
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin()
+	// Make sure to close the plugin when you're done. Otherwise the plugin will
+	// keep running, which shows the cancel button at the bottom of the screen.
+	figma.closePlugin()
 }
 
 figma.on("selectionchange", postSelectionCountUpdate)
 
 function postSelectionCountUpdate() {
-  console.log(figma.currentPage.selection.map(({ id }) => id))
+	console.log(figma.currentPage.selection.map(({ id }) => id))
 
-  const numSelected = figma.currentPage.selection.length
-  figma.ui.postMessage({
-    type: "selection-count-update",
-    count: numSelected,
-  })
+	const numSelected = figma.currentPage.selection.length
+	figma.ui.postMessage({
+		type: "selection-count-update",
+		count: numSelected,
+	})
 }
 
 /**
- * @returns The scene nodes in the current selection, sorted front to back (top to bottom in the scene tree UI).
+ * @returns The scene nodes in the current selection, sorted front to back (top to bottom in the layers panel UI).
  */
 function getSortedSelection(): SceneNode[] {
-  const selectionIds = new Set(
-    figma.currentPage.selection.map((node) => node.id),
-  )
+	const selectionIds = new Set(
+		figma.currentPage.selection.map((node) => node.id),
+	)
 
-  const ordered: SceneNode[] = []
+	const ordered: SceneNode[] = []
 
-  function walk(node: BaseNode) {
-    if (selectionIds.has(node.id) && node.type !== "PAGE") {
-      ordered.push(node as SceneNode)
-    }
-    if ("children" in node) {
-      // loop through node children in reverse order,
-      // BECAUSE the `children` property lists the layers sorted back to front (bottom to top in scene tree ui)
-      for (let i = node.children.length - 1; i >= 0; i--) {
-        const child = node.children[i]
-        walk(child)
-      }
-    }
-  }
+	function walk(node: BaseNode) {
+		if (selectionIds.has(node.id) && node.type !== "PAGE") {
+			ordered.push(node as SceneNode)
+			selectionIds.delete(node.id)
+		}
 
-  // walk along the node hierarchy of the current page in a depth-first manner
-  // and get the nodes in that order
-  walk(figma.currentPage)
+		if (selectionIds.size === 0) {
+			return
+		}
 
-  return ordered
+		if ("children" in node) {
+			// loop through node children in reverse order,
+			// BECAUSE the `children` property lists the layers sorted back to front (bottom to top in layers panel ui)
+			for (let i = node.children.length - 1; i >= 0; i--) {
+				const child = node.children[i]
+				walk(child)
+			}
+		}
+	}
+
+	// walk along the node hierarchy of the current page in a depth-first manner
+	// and get the nodes in that order
+	walk(figma.currentPage)
+
+	return ordered
 }
